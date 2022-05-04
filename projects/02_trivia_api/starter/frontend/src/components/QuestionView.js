f8ce429e15a6a4bed9cb41bb5e45be5a205b1b31
Neelima007 <neelu.arangi@gmail.com>
2022-05-04 10:48:40 +0530

    Initial commit

--- projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
+++ projects/02_trivia_api/starter/frontend/src/components/QuestionView.js
@@ -26,7 +26,9 @@ class QuestionView extends Component {
       url: `/questions?page=${this.state.page}`, //TODO: update request URL
       type: "GET",
       success: (result) => {
+       
         this.setState({
+
           questions: result.questions,
           totalQuestions: result.total_questions,
           categories: result.categories,
@@ -76,7 +78,7 @@ class QuestionView extends Component {
 
   submitSearch = (searchTerm) => {
     $.ajax({
-      url: `/questions`, //TODO: update request URL
+      url: `/questions/search`, //TODO: update request URL
       type: "POST",
       dataType: 'json',
       contentType: 'application/json',
